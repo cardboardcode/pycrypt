7c9ceee2c9ef569fce68fd25c2ce7486a3fe8ab0
cardboardcode <cardboardmails@gmail.com>
2017-02-25 10:07:28 +0800

    Added encryption lexicon for capital letters. Closes #2, #9

--- src/decode.py
+++ src/decode.py
@@ -83,7 +83,59 @@ for letter in text:
 		st+=str('z')		
 	if (letter == ' '):
 		st+=str(' ')
-
+	#Capital Letters Lexicon start
+	if (letter == 'A'):
+		st+=str('Z')
+	if (letter == 'B'):
+		st+=str('Y')
+	if (letter == 'C'):
+		st+=str('W')
+	if (letter == 'D'):
+		st+=str('V')
+	if (letter == 'E'):
+		st+=str('U')
+	if (letter == 'F'):
+		st+=str('T')
+	if (letter == 'G'):
+		st+=str('S')
+	if (letter == 'H'):
+		st+=str('R')
+	if (letter == 'I'):
+		st+=str('Q')
+	if (letter == 'J'):
+		st+=str('P')
+	if (letter == 'K'):
+		st+=str('O')
+	if (letter == 'L'):
+		st+=str('N')
+	if (letter == 'M'):
+		st+=str('M')
+	if (letter == 'N'):
+		st+=str('L')
+	if (letter == 'O'):
+		st+=str('I')
+	if (letter == 'P'):
+		st+=str('H')
+	if (letter == 'Q'):
+		st+=str('J')
+	if (letter == 'R'):
+		st+=str('H')
+	if (letter == 'S'):
+		st+=str('G')
+	if (letter == 'T'):
+		st+=str('F')
+	if (letter == 'U'):
+		st+=str('E')
+	if (letter == 'V'):
+		st+=str('D')
+	if (letter == 'W'):
+		st+=str('C')
+	if (letter == 'Y'):
+		st+=str('B')
+	if (letter == 'Z'):
+		st+=str('A')
+		
+		
 file.write(st)
 
 #Time Recording Start
